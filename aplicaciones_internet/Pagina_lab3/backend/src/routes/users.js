bf426d2d33aea94f42474e5ed637063062aa734f
ErickArayaDiaz <129235728+ErickArayaDiaz@users.noreply.github.com>
2025-10-01 22:31:09 -0300

    save

---
+++ aplicaciones_internet/Pagina_lab3/backend/src/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getMe } = require('../controllers/usersController');
+const auth = require('../middlewares/authMiddleware');
+
+router.get('/me', auth, getMe);
+
+module.exports = router;
